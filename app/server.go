ba72db9735d44810a319a945c3a65bf7c9868a12
张铭奇 <2907276798@qq.com>
2022-11-15 10:00:47 +0800

    Handle concurrent clients

--- app/server.go
+++ app/server.go
@@ -13,17 +13,27 @@ func main() {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
-	accept, err := listen.Accept() // start port accept
-	defer accept.Close()           // close port listen
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
+
+	// close port listen
+
+	for {
+		conn, err := listen.Accept() // start port accept
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			os.Exit(1)
+		}
+		go handleConnection(conn)
 	}
+
+}
+
+func handleConnection(conn net.Conn) {
+	defer conn.Close()
+
 	for {
 		buf := make([]byte, 1024)
 		// try to read accept data
-		n, err := accept.Read(buf)
-		if err != nil {
+		if _, err := conn.Read(buf); err != nil {
 			if err == io.EOF {
 				break
 			} else {
@@ -31,9 +41,7 @@ func main() {
 				os.Exit(1)
 			}
 		}
-		println(n)
 		// accept successful , response a data
-		accept.Write([]byte("PONG\r\n"))
+		conn.Write([]byte("+PONG\r\n"))
 	}
-
 }
